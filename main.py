b700354da2830c2082c70a26845dcdd5332d48bf
Carlos <carlrd99@gmail.com>
2022-06-14 13:07:31 +0200

    checkpoint4

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import csv
+from pydoc import describe
 from tkinter.tix import Tree
 
 def read_data(filename1, filename2):
@@ -36,13 +37,33 @@ def get_name_description(key, dic):
 
 def search_by_lon(lon, dic):
     lon_list=[]
+    clave=""
     for k in dic.keys():
         dic_interno=dic[k]
-        lon_list.append(dic_interno["lon"])
-    print(lon_list)
+        lon_list.append(float(dic_interno["lon"]))
+        if(float(dic_interno["lon"])==lon):
+            clave = k
+    if str(type(lon)) != "<class 'float'>":
+        raise ValueError("No es de tipo float")
+    elif lon not in lon_list == True:
+        raise ValueError("No existe")
+    else:
+        return clave
+def get_min(key, dic):
+    dic_list=[]
+    for k in dic.keys():
+        if float(k)<float(key):
+            name,desc=get_name_description(k,dic)
+            dic_res={name:desc}
+            dic_list.append(dic_res)
+    if k < min(dic.keys()):
+        raise ValueError("No hay elementos")
+    print(dic_list)
+
 
 if __name__ == "__main__":
     dic=read_data("stops.csv","stops_data.csv")
+    print(dic)
     try:
         name, desc = get_name_description("1020",dic)
         print(name, desc)
@@ -50,11 +71,22 @@ if __name__ == "__main__":
     except ValueError:
         print("Ha saltado error")
     
-    try:
+    """try:
         name, desc = get_name_description("2020",dic)
         print(name, desc)
 
     except ValueError:
-        print("Ha saltado error")
+        print("Ha saltado error")"""
 
-    search_by_lon(1.0, dic)
+    try:
+        k=search_by_lon(-0.366829060109773, dic)
+        print(k)
+    except ValueError as err:
+        if str(err)=="No existe":
+            print("Error de clave")
+        elif str(err)=="No es de tipo float":
+            print("Error de tipo")
+    try:
+        get_min(1023, dic)
+    except ValueError:
+        print("Ha saltado error")
